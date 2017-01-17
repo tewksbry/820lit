402d06df62f693ebcd609bd8d48e660cdbd134e3
Stiven Deleur <deleur@usc.edu>
2017-01-16 16:29:15 -0800

    made sound visualizer launch only if its the main module

--- sound_visualizer.py
+++ sound_visualizer.py
@@ -1,4 +1,4 @@
-#Author: Samuel Resendez
+# Author: Samuel Resendez
 
 import pyaudio
 import time
@@ -8,51 +8,48 @@ from matplotlib import pyplot as plt
 import scipy.signal as signal
 
 
-
 CHANNELS = 1
 RATE = 8000
 CHUNK = 130
-#60 per second
-
+# 60 per second
 
 
 p = pyaudio.PyAudio()
 curr_average = 0
 
 
-
 def sigmoid(x):
-    return int(240 / (1 + 0.5 * math.exp( -0.001 * x)))
+    return int(240 / (1 + 0.5 * math.exp(-0.001 * x)))
 
 
 def callback(in_data, frame_count, time_info, flag):
     global curr_average
     audio_data = np.fromstring(in_data, dtype=np.int16)
 
-    print(sigmoid(max(audio_data)));
+    print(sigmoid(max(audio_data)))
 
-
-    #do processing here
+    # do processing here
 
     return (audio_data, pyaudio.paContinue)
 
 
 def main():
     stream = p.open(format=pyaudio.paInt16,
-                channels=CHANNELS,
-                rate=RATE,
-                frames_per_buffer= CHUNK,
-                input=True,
-                stream_callback=callback)
+                    channels=CHANNELS,
+                    rate=RATE,
+                    frames_per_buffer=CHUNK,
+                    input=True,
+                    stream_callback=callback)
 
     stream.start_stream()
 
     while stream.is_active():
-       time.sleep(5)
-       stream.stop_stream()
+        time.sleep(5)
+        stream.stop_stream()
 
     stream.close()
 
     p.terminate()
 
-main()
+if __name__ == "__main__":
+    main()
