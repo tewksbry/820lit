2180be71d42c60cecdc814178e5c0abcd60044db
Samuel Resendez <saresend@usc.edu>
2017-01-18 20:16:37 -0800

    spelling

--- desktop_script.py
+++ desktop_script.py
@@ -33,7 +33,7 @@ def main():
 
     handler = soundHandler()
 
-    def new_pattern(volume, frequency,patterNumber):
+    def new_pattern(volume, frequency, pattern):
         global last_volume
         next_pattern = patternCreator(volume=volume)
         last_volume = volume
