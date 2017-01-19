1b6e95cdbef295e7a1e2216dc4ea85b54025c452
Stiven Deleur <deleur@usc.edu>
2017-01-18 20:37:33 -0800

    merge

--- desktop_script.py
+++ desktop_script.py
@@ -13,7 +13,7 @@ next_pattern = Pattern([LED() for _ in range(PIXEL_NUM)])
 last_volume = 0
 
 
-def patternCreator(volume=0):
+def patternCreator(volume=0, frequency=0):
     return pattern.middleOut(volume, previous=next_pattern, fade=0.95, cutoff=1, fill=False, color_palette=pattern.rotatedRainbow, last_volume=last_volume)
 
 
@@ -36,7 +36,7 @@ def main():
 
     def new_pattern(volume, frequency, pattern):
         global last_volume
-        next_pattern = patternCreator(volume=volume)
+        next_pattern = patternCreator(volume=volume, frequency=frequency)
         last_volume = volume
         visualizer.update(next_pattern)
         print(frequency)
