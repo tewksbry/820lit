175e14a16d8c09666417dea6701b626c5615ccc7
Stiven Deleur <deleur@usc.edu>
2017-02-02 21:52:09 -0800

    testing

--- desktop_script.py
+++ desktop_script.py
@@ -90,7 +90,8 @@ def main():
 
     def setParam(key, value):
         if key not in param_dict or param_dict[key] != value:
-            print("Updating parameter:", key, "from: ", param_dict[key], "to: ", value)
+            if key in param_dict:
+                print("Updating parameter:", key, "from: ", param_dict[key], "to: ", value)
             passParam(ser, key, *value)
             param_dict[key] = value
             print("New parameters:", param_dict)
